2c8284d45bac7288962b98c0948f9138c7888694
unknown <sourabhghorpade16@gmail.com>
2019-02-16 12:52:45 +0530

    Weather

--- bin/controllers/config/config.js
+++ bin/controllers/config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     jwtSecret: 'long-live-the-ionic-academy',
-    db: 'mongodb://localhost/ClientDB'
+    db: 'mongodb://localhost/ClientDB',
+    WeatherapiKey:'66093c60a939d14ecda181a15c3c2f5c'
 };
